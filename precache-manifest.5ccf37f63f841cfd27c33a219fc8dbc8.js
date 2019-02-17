09a3d773c0074a080614ce70bdeb63a744ab3351
Max <max.oppor1@gmail.com>
2019-02-17 15:21:56 -0600

    Updates

---
+++ precache-manifest.5ccf37f63f841cfd27c33a219fc8dbc8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2038828b584b4d902d8b16a23f7b210",
+    "url": "/my-uis/static/media/smaller.f2038828.jpg"
+  },
+  {
+    "revision": "84c1a1d30c620d3ae1ef64c71b654498",
+    "url": "/my-uis/static/media/redhead.84c1a1d3.png"
+  },
+  {
+    "revision": "c19572700d051eb133d09092542b636f",
+    "url": "/my-uis/static/media/ombre.c1957270.jpg"
+  },
+  {
+    "revision": "86cc81ac829a989515af0922908857f0",
+    "url": "/my-uis/static/media/blonde.86cc81ac.png"
+  },
+  {
+    "revision": "771f047015a453ecb3b3",
+    "url": "/my-uis/static/js/runtime~main.771f0470.js"
+  },
+  {
+    "revision": "519601fcf9085ffe6ba0",
+    "url": "/my-uis/static/js/main.519601fc.chunk.js"
+  },
+  {
+    "revision": "9a9d7d54b724089aac22",
+    "url": "/my-uis/static/js/2.9a9d7d54.chunk.js"
+  },
+  {
+    "revision": "519601fcf9085ffe6ba0",
+    "url": "/my-uis/static/css/main.433a6504.chunk.css"
+  },
+  {
+    "revision": "dca2fdc88babf44e88fc790817d4901a",
+    "url": "/my-uis/index.html"
+  }
+];
